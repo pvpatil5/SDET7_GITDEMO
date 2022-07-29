c3098c13e57c7fb35ef5e352cdd6b1868f80c751
pvpatil5 <pvpatil123@yahoo.com>
2022-07-29 12:55:40 +0530

    commiting to camp branch

---
+++ src/test/java/campaigns/Camp1.java
@@ -0,0 +1,10 @@
+package campaigns;
+
+public class Camp1 {
+
+	public static void main(String[] args) {
+		System.out.println("Camp1 Class");
+
+	}
+
+}
---
+++ src/test/java/campaigns/Camp2.java
@@ -0,0 +1,10 @@
+package campaigns;
+
+public class Camp2 {
+
+	public static void main(String[] args) {
+		System.out.println("Camp2 Class");
+
+	}
+
+}
