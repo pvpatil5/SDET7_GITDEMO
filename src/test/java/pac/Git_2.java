0ac208bfc87e4590a709be74163f3476513d145f
pvpatil5 <pvpatil123@yahoo.com>
2022-07-29 12:25:23 +0530

    Second Commit

--- src/test/java/pac/Git_2.java
+++ src/test/java/pac/Git_2.java
@@ -4,6 +4,7 @@ public class Git_2 {
 
 	public static void main(String[] args) {
 		System.out.println("Git_2 class");
+		System.out.println("Git_2 class");
 
 	}
 
---
+++ src/test/java/pac/Git_3.java
@@ -0,0 +1,10 @@
+package pac;
+
+public class Git_3 {
+
+	public static void main(String[] args) {
+		System.out.println("Git_3 class");
+
+	}
+
+}
