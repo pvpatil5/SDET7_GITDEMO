97bb3a5f774a6d605eff0d8081bda3fd9e077d54
pinja <pinja@DESKTOP-3BU923C>
2022-07-29 15:30:30 +0530

    Irfan updated

---
+++ src/test/java/IRFAN/Sample_1.java
@@ -0,0 +1,10 @@
+package IRFAN;
+
+public class Sample_1 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("sample_1");
+	}
+
+}
---
+++ src/test/java/IRFAN/Sample_2.java
@@ -0,0 +1,9 @@
+package IRFAN;
+
+public class Sample_2 {
+	
+	public static void main(String[] args) {
+		System.out.println("sample_2");
+	}
+
+}
