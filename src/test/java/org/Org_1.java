a9881597c3464accf142264e389e39c09a4007a6
pvpatil5 <pvpatil123@yahoo.com>
2022-07-29 12:45:53 +0530

    Commiting to Org Branch

---
+++ src/test/java/org/Org_1.java
@@ -0,0 +1,10 @@
+package org;
+
+public class Org_1 {
+
+	public static void main(String[] args) {
+		System.out.println("Org_1 class");
+
+	}
+
+}
---
+++ src/test/java/org/Org_2.java
@@ -0,0 +1,10 @@
+package org;
+
+public class Org_2 {
+
+	public static void main(String[] args) {
+		System.out.println("Org_2 class");
+
+	}
+
+}
